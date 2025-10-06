a176e34127bce2a85f7d0ba0096dbee4cd81736e
“Michael <240designworks@gmail.com>
2025-10-05 23:51:17 -0500

    Initial Commit

---
+++ DataPowerSync/DataPowerSyncApp.swift
@@ -0,0 +1,32 @@
+//
+//  DataPowerSyncApp.swift
+//  DataPowerSync
+//
+//  Created by Michael Martell on 10/5/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DataPowerSyncApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
